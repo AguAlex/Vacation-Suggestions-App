8b9c1000b508fb439cf5f06df98527519e888ad3
Agusoaei Alexandru <alex.agu@yahoo.com>
2025-03-24 12:59:49 +0200

    top countries in home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import "./Home.css";
+import country1Img from "../assets/us.jpg";
 
 function Home({ user, setUser }) {
   const navigate = useNavigate();
-
+  const [countries, setCountries] = useState([]);
   useEffect(() => {
     const token = localStorage.getItem("token");
     const storedUser = localStorage.getItem("user");
@@ -25,11 +26,21 @@ function Home({ user, setUser }) {
   }, [setUser]);
   
 
-  // const handleLogout = () => {
-  //   localStorage.removeItem("token");
-  //   localStorage.removeItem("user");
-  //   navigate("/login");
-  // };
+  useEffect(() => {
+    const fetchCountries = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/countries");
+        if (!response.ok) throw new Error("Failed to fetch countries");
+        const data = await response.json();
+        console.log(data);
+        setCountries(data.slice(0, 3));
+      } catch (error) {
+        console.error("Error fetching countries:", error);
+      }
+    };
+
+    fetchCountries();
+  }, []);
 
   return (
     <div className="home-container">
@@ -44,6 +55,22 @@ function Home({ user, setUser }) {
           <h1>Hello! You can sign up.</h1>
         </div>
       )}
+
+      <h2>Top Countries</h2>
+      <div className="countries-container">
+        {countries.length > 0 ? (
+          countries.map((country) => (
+            <div className="country-card" key={country.id}>
+              <h3>{country.name}</h3>
+              {/* Dacă ai poze sau descriere, le pui aici */}
+              <img src={country1Img} alt="Country 1" />
+              
+            </div>
+          ))
+        ) : (
+          <p>Loading countries...</p>
+        )}
+      </div>
     </div>
   );
 }
