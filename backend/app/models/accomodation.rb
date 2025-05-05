1dfc9d5bb15e9229f8c1122d4942ea43b0d3f29e
Agusoaei Alexandru <alex.agu@yahoo.com>
2025-05-05 11:22:44 +0300

    MyLikes page

--- backend/app/models/accomodation.rb
+++ backend/app/models/accomodation.rb
@@ -4,9 +4,10 @@ class Accomodation < ApplicationRecord
   has_many :users, through: :accomodations_users
 
   has_many :likes
-  has_many :liked_users, through: :likes, source: :user
+  has_many :liked_by_users, through: :likes, source: :user
+
 
   def likes_count
-    likes.count
+    likes.size
   end
 end
