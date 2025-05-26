16ccfe53b1b9ecb1c4b456b0f2c8016a49a6f0a8
Agusoaei Alexandru <alex.agu@yahoo.com>
2025-05-26 13:41:18 +0300

    steaguri si recomandari

--- backend/app/controllers/recommended_controller.rb
+++ backend/app/controllers/recommended_controller.rb
@@ -2,30 +2,42 @@ class RecommendedController < ApplicationController
   before_action :set_user
 
   def recommended
-    # Pas 1: Obține utilizatorul
-    user = @user  # Păstrează obiectul, nu doar id-ul
+    user = @user
 
-    # Pas 2: Extrage toate city_id-urile din accommodations pe care le-a dat like
     city_ids = user.likes
-                   .includes(:accomodation)
-                   .map { |like| like.accomodation.city_id }
-                   .uniq
+                  .includes(:accomodation)
+                  .map { |like| like.accomodation.city_id }
+                  .uniq
 
-    # Obține orașele corespunzătoare city_id-urilor
     cities = City.where(id: city_ids)
     city_codes = cities.pluck(:iata_code).compact.uniq
 
     access_token = AmadeusService.get_access_token
-    data = []
-    
+    raw_data = []
+
     city_codes.each do |code|
       recommendations = AmadeusService.get_recommendations_by_city_code(code, access_token)
-      data.concat(recommendations) if recommendations.present?
+      raw_data.concat(recommendations) if recommendations.present?
     end
-    data.uniq! { |item| item["iataCode"] }
-    
-    if data.any?
-      render json: data
+
+    enriched_data = raw_data.uniq { |item| item["iataCode"] }.map do |item|
+      iata = item["iataCode"]
+      local_city = City.includes(:country).find_by(iata_code: iata)
+
+      if local_city && local_city.country
+        country = local_city.country
+        item.merge(
+          "country_name" => country.name,
+          "country_id" => country.id,
+          "country_image" => country.image
+        )
+      else
+        item
+      end
+    end
+
+    if enriched_data.any?
+      render json: enriched_data
     else
       render json: { error: "Failed to fetch any recommendations" }, status: :bad_gateway
     end
